6428b3d76f4b9d2bd4e0b5202858d3f4f04fcfc8
Marcus Westin <marcus.westin@gmail.com>
2010-06-27 14:16:31 -0700

    Rename internal store to storage to avoid confusion

--- store.js
+++ store.js
@@ -3,7 +3,7 @@ var store = (function(){
 		win = window,
 		doc = win.document,
 		name = 'localStorage',
-		store
+		storage
 	
 	api.set = function(key, value) {}
 	api.get = function(key) {}
@@ -11,43 +11,43 @@ var store = (function(){
 	api.clear = function() {}
 	
 	if (win.globalStorage) {
-		store = win.globalStorage[win.location.hostname]
-		api.set = function(key, val) { store[key] = val }
-		api.get = function(key) { return store[key].value }
-		api.delete = function(key) { delete store[key] }
-		api.clear = function() { for (var key in store ) { delete store[key] } }
+		storage = win.globalStorage[win.location.hostname]
+		api.set = function(key, val) { storage[key] = val }
+		api.get = function(key) { return storage[key].value }
+		api.delete = function(key) { delete storage[key] }
+		api.clear = function() { for (var key in storage ) { delete storage[key] } }
 	} else if (win.localStorage) {
-		store = win.localStorage
-		api.set = function(key, val) { store[key] = val }
-		api.get = function(key) { return store[key] }
-		api.delete = function(key) { delete store[key] }
-		api.clear = function() { for (var key in store ) { delete store[key] } }
+		storage = win.localStorage
+		api.set = function(key, val) { storage[key] = val }
+		api.get = function(key) { return storage[key] }
+		api.delete = function(key) { delete storage[key] }
+		api.clear = function() { for (var key in storage ) { delete storage[key] } }
 	} else if (Element.prototype.addBehavior) {
-		store = doc.body.appendChild(doc.createElement('div'))
-		store.style.display = 'none'
+		storage = doc.body.appendChild(doc.createElement('div'))
+		storage.style.display = 'none'
 		// See http://msdn.microsoft.com/en-us/library/ms531081(v=VS.85).aspx
 		// and http://msdn.microsoft.com/en-us/library/ms531424(v=VS.85).aspx
-		store.addBehavior('#default#userData') 
-		store.load(name)
+		storage.addBehavior('#default#userData') 
+		storage.load(name)
 		api.set = function(key, val) {
-			store.setAttribute(key, val)
-			store.save(name)
+			storage.setAttribute(key, val)
+			storage.save(name)
 		}
 		api.get = function(key) {
-			return store.getAttribute(key)
+			return storage.getAttribute(key)
 		}
 		api.delete = function(key) {
-			store.removeAttribute(key)
-			store.save(name)
+			storage.removeAttribute(key)
+			storage.save(name)
 		}
 		api.clear = function() {
-			var attributes = store.XMLDocument.documentElement.attributes;
+			var attributes = storage.XMLDocument.documentElement.attributes;
 			for (var i=0, attr; attr = attributes[i]; i++) {
-				store.removeAttribute(attr.name)
+				storage.removeAttribute(attr.name)
 			}
-			store.save(name)
+			storage.save(name)
 		}
 	}
 	
 	return api
-})();
+})()
