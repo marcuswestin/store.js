73878e04c40a594cbf28a6e1a8733fc9984d9441
Steven Black <steveb@stevenblack.com>
2010-12-07 23:50:48 -0500

    Ran store.js through lint

--- store.js
+++ store.js
@@ -25,77 +25,77 @@ var store = (function(){
 		doc = win.document,
 		localStorageName = 'localStorage',
 		globalStorageName = 'globalStorage',
-		storage
+		storage;
 	
-	api.set = function(key, value) {}
-	api.get = function(key) {}
-	api.remove = function(key) {}
-	api.clear = function() {}
+	api.set = function(key, value) {};
+	api.get = function(key) {};
+	api.remove = function(key) {};
+	api.clear = function() {};
 	api.transact = function(key, transactionFn) {
-		var val = api.get(key)
-		if (typeof val == 'undefined') { val = {} }
-		transactionFn(val)
-		api.set(key, val)
-	}
+		var val = api.get(key);
+		if (typeof val == 'undefined') { val = {}; }
+		transactionFn(val);
+		api.set(key, val);
+	};
 	
 	api.serialize = function(value) {
-		return JSON.stringify(value)
-	}
+		return JSON.stringify(value);
+	};
 	api.deserialize = function(value) {
-		if (typeof value != 'string') { return undefined }
-		return JSON.parse(value)
-	}
+		if (typeof value != 'string') { return undefined; }
+		return JSON.parse(value);
+	};
 	
 	if (localStorageName in win && win[localStorageName]) {
-		storage = win[localStorageName]
-		api.set = function(key, val) { storage.setItem(key, api.serialize(val)) }
-		api.get = function(key) { return api.deserialize(storage.getItem(key)) }
-		api.remove = function(key) { storage.removeItem(key) }
-		api.clear = function() { storage.clear() }
+		storage = win[localStorageName];
+		api.set = function(key, val) { storage.setItem(key, api.serialize(val)); };
+		api.get = function(key) { return api.deserialize(storage.getItem(key)); };
+		api.remove = function(key) { storage.removeItem(key); };
+		api.clear = function() { storage.clear(); };
 
 	} else if (globalStorageName in win && win[globalStorageName]) {
-		storage = win[globalStorageName][win.location.hostname]
-		api.set = function(key, val) { storage[key] = api.serialize(val) }
-		api.get = function(key) { return api.deserialize(storage[key] && storage[key].value) }
-		api.remove = function(key) { delete storage[key] }
-		api.clear = function() { for (var key in storage ) { delete storage[key] } }
+		storage = win[globalStorageName][win.location.hostname];
+		api.set = function(key, val) { storage[key] = api.serialize(val); };
+		api.get = function(key) { return api.deserialize(storage[key] && storage[key].value); };
+		api.remove = function(key) { delete storage[key]; };
+		api.clear = function() { for (var key in storage ) { delete storage[key]; } };
 
 	} else if (doc.documentElement.addBehavior) {
-		var storage = doc.createElement('div')
+		storage = doc.createElement('div');
 		function withIEStorage(storeFunction) {
 			return function() {
-				var args = Array.prototype.slice.call(arguments, 0)
-				args.unshift(storage)
+				var args = Array.prototype.slice.call(arguments, 0);
+				args.unshift(storage);
 				// See http://msdn.microsoft.com/en-us/library/ms531081(v=VS.85).aspx
 				// and http://msdn.microsoft.com/en-us/library/ms531424(v=VS.85).aspx
-				doc.body.appendChild(storage)
-				storage.addBehavior('#default#userData')
-				storage.load(localStorageName)
-				var result = storeFunction.apply(api, args)
-				doc.body.removeChild(storage)
-				return result
-			}
+				doc.body.appendChild(storage);
+				storage.addBehavior('#default#userData');
+				storage.load(localStorageName);
+				var result = storeFunction.apply(api, args);
+				doc.body.removeChild(storage);
+				return result;
+			};
 		}
 		api.set = withIEStorage(function(storage, key, val) {
-			storage.setAttribute(key, api.serialize(val))
-			storage.save(localStorageName)
-		})
+			storage.setAttribute(key, api.serialize(val));
+			storage.save(localStorageName);
+		});
 		api.get = withIEStorage(function(storage, key) {
-			return api.deserialize(storage.getAttribute(key))
-		})
+			return api.deserialize(storage.getAttribute(key));
+		});
 		api.remove = withIEStorage(function(storage, key) {
-			storage.removeAttribute(key)
-			storage.save(localStorageName)
-		})
+			storage.removeAttribute(key);
+			storage.save(localStorageName);
+		});
 		api.clear = withIEStorage(function(storage) {
-			var attributes = storage.XMLDocument.documentElement.attributes
-			storage.load(localStorageName)
+			var attributes = storage.XMLDocument.documentElement.attributes;
+			storage.load(localStorageName);
 			for (var i=0, attr; attr = attributes[i]; i++) {
-				storage.removeAttribute(attr.name)
+				storage.removeAttribute(attr.name);
 			}
-			storage.save(localStorageName)
-		})
+			storage.save(localStorageName);
+		});
 	}
 
-	return api
+	return api;
 })();
