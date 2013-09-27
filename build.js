a3e519dba619d15275276105a279b09a8e892bd8
Marcus Westin <marcus.westin@gmail.com>
2013-09-27 13:54:34 -0700

    Update copyright

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ var fs = require('fs'),
 
 var storeJS = fs.readFileSync(__dirname + '/store.js').toString(),
 	jsonJS = fs.readFileSync(__dirname + '/json.js').toString(),
-	copy = '/* Copyright (c) 2010-2012 Marcus Westin */'
+	copy = '/* Copyright (c) 2010-2013 Marcus Westin */'
 
 console.log('building and minifying...')
 buildFile(storeJS, 'store.min.js')
