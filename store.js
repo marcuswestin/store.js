666ef71b21655d84fc7b10d454a13535d7e28405
Marcus Westin <marcus.westin@gmail.com>
2014-11-23 20:42:37 -0500

    Add store.version

--- store.js
+++ store.js
@@ -6,6 +6,7 @@
 		storage
 
 	store.disabled = false
+	store.version = '1.3.17'
 	store.set = function(key, value) {}
 	store.get = function(key, defaultVal) {}
 	store.has = function(key) { return store.get(key) !== undefined }
