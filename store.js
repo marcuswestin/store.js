85f7a215c0764fe73110688e7bf66c4d73f242e5
Ezequiel Bergamaschi <ebergamaschi@medallia.com>
2015-10-08 16:24:40 -0700

    adding id to module definition

--- store.js
+++ store.js
@@ -4,7 +4,7 @@
 ;(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define([], factory);
+        define('store', [], factory);
     } else if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
