64a46ab856814906611056625edf808c0c02c704
Marcus Westin <marcus.westin@gmail.com>
2017-02-21 13:45:34 -0500

    Simulate localStorage in node test runner

--- scripts/run-node-tests.js
+++ scripts/run-node-tests.js
@@ -1,8 +1,11 @@
 #!/usr/local/bin/node
 
+// simulate localStorage - must be done before importing tests
+var { global } = require('../src/util')
 var LocalStorage = require('node-localstorage').LocalStorage
-var tests = require('../tests/tests')
-
 global.localStorage = new LocalStorage('/tmp/store.js-test')
+
+// Import and run tests
+var tests = require('../tests/tests')
 
 tests.runTests()
