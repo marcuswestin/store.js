521c096f690fcc1b2e74ebb8d773ede61dda8005
Marcus Westin <marcus.westin@gmail.com>
2017-02-21 13:46:09 -0500

    Fix expire_test, which was failing in browser

--- src/addon/expire_test.js
+++ src/addon/expire_test.js
@@ -7,21 +7,22 @@ module.exports = {
 function setup(store) {
 
 	test('expire', function(done) {
-		var duration = 30
-		store.set('foo', 'bar', new Date().getTime() + duration)
+		var duration = 200
+		var expiration = new Date().getTime() + duration
+		store.set('foo', 'bar', expiration)			
 		assert(store.get('foo') == 'bar')
 		setTimeout(function() {
+			assert(new Date().getTime() < expiration)
 			assert(store.get('foo') == 'bar')
-		}, duration/2)
-		setTimeout(function() {
-			assert(store.get('foo') == undefined)
-			store.set('foo', 'bar')
 			setTimeout(function() {
-				assert(store.get('foo') == 'bar')
-				done()
-			}, 5)
-		}, duration)
-		
+				assert(new Date().getTime() > expiration)
+				assert(store.get('foo') == undefined)
+				store.set('foo', 'bar')
+				setTimeout(function() {
+					assert(store.get('foo') == 'bar')
+					done()
+				}, 5)
+			}, duration)
+		}, duration/10)
 	})
-
 }
