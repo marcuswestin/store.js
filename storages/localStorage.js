8c6621d0fab012fb53c607ad7fdf36326b1e3e24
Marcus Westin <marcus.westin@gmail.com>
2017-02-28 11:10:59 -0500

    Fix #190 - by accessing localStorage outside of the try/catch block in store-engine.js _testStorage (https://github.com/marcuswestin/store.js/blob/eccd5cdcdce45a1e054de25eab2d2cdd2ed1e507/src/store-engine.js#L147) the access error was not being caught

--- storages/localStorage.js
+++ storages/localStorage.js
@@ -9,27 +9,29 @@ module.exports = {
 	clearAll: clearAll,
 }
 
-var localStorage = Global.localStorage
+function localStorage() {
+	return Global.localStorage
+}
 
 function read(key) {
-	return localStorage.getItem(key)
+	return localStorage().getItem(key)
 }
 
 function write(key, data) {
-	return localStorage.setItem(key, data)
+	return localStorage().setItem(key, data)
 }
 
 function each(fn) {
-	for (var i = localStorage.length - 1; i >= 0; i--) {
-		var key = localStorage.key(i)
+	for (var i = localStorage().length - 1; i >= 0; i--) {
+		var key = localStorage().key(i)
 		fn(read(key), key)
 	}
 }
 
 function remove(key) {
-	return localStorage.removeItem(key)
+	return localStorage().removeItem(key)
 }
 
 function clearAll() {
-	return localStorage.clear()
+	return localStorage().clear()
 }
--- storages/sessionStorage.js
+++ storages/sessionStorage.js
@@ -9,27 +9,29 @@ module.exports = {
 	clearAll: clearAll,
 }
 
-var sessionStorage = Global.sessionStorage
+function sessionStorage() {
+	return Global.sessionStorage
+}
 
 function read(key) {
-	return sessionStorage.getItem(key)
+	return sessionStorage().getItem(key)
 }
 
 function write(key, data) {
-	return sessionStorage.setItem(key, data)
+	return sessionStorage().setItem(key, data)
 }
 
 function each(fn) {
-	for (var i = sessionStorage.length - 1; i >= 0; i--) {
-		var key = sessionStorage.key(i)
+	for (var i = sessionStorage().length - 1; i >= 0; i--) {
+		var key = sessionStorage().key(i)
 		fn(read(key), key)
 	}
 }
 
 function remove(key) {
-	return sessionStorage.removeItem(key)
+	return sessionStorage().removeItem(key)
 }
 
 function clearAll() {
-	return sessionStorage.clear()
+	return sessionStorage().clear()
 }
