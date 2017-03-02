a14028d0d51a0fad5ec274c019a7ca678b407ddc
Joe Fleming <joe.fleming@gmail.com>
2017-03-02 16:43:17 -0700

    update tests

--- plugins/compression_test.js
+++ plugins/compression_test.js
@@ -6,28 +6,31 @@ module.exports = {
 }
 
 function setup(store) {
+	var raw = true
 
-	test('compression', function() {
-		store.compress('foo', 'baz')
-		assert(store.get('foo') == 'ᄂゆ׬䀀', 'value should be lz compressed')
-		assert(store.decompress('foo') == 'baz', 'value should be baz')
+	test('string compression', function() {
+		store.set('foo', 'baz')
+		assert(store.get('foo', raw) == 'ᄂゆ׬䀀', 'string should be lz compressed')
+		assert(store.get('foo') == 'baz', 'value should be baz')
+	})
+
+	test('object compression', function () {
 		var obj = { one: { two: 3 }}
-		store.compress('foo', obj)
-		assert(store.get('foo') == '㞂⃶ݠ꘠岠ᜃ릎٠⾆耀', 'object should be lz compressed')
-		assert(util.deepEqual(store.decompress('foo'), obj), 'should deep equal original object')
+		store.set('foo', obj)
+		assert(store.get('foo', raw) == '㞂⃶ݠ꘠岠ᜃ릎٠⾆耀', 'object should be lz compressed')
+		assert(util.deepEqual(store.get('foo'), obj), 'should deep equal original object')
 		store.remove('foo')
 	})
 
-	test('compression clobbers existing values', function () {
-		store.set('foo', 'bar')
-		assert(store.get('foo') == 'bar', 'value should be bar')
-		store.compress('foo', 'baz')
-		assert(store.decompress('foo') == 'baz', 'new value should be baz')
+	test('decompress uncopmressed data', function () {
+		// fake uncompressed data by storing raw value
+		store.set('foo', 'baz', raw)
+		assert(store.get('foo') == 'baz', 'value should be baz')
 		store.remove('foo')
 	})
 
-	test('decompressing non-existing data', function () {
-		assert(store.decompress('bar') == undefined, 'value should be undefined')
+	test('decompress non-existing data', function () {
+		assert(store.get('bar') == undefined, 'value should be undefined')
 		store.remove('bar')
 	})
 
