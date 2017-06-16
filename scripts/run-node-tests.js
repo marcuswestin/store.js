a652639ebfd2f002b7ff0f51e1c14ff3893bf72a
Marcus Westin <marcus.westin@gmail.com>
2017-06-16 14:33:15 -0400

    Use localStorage instead of node-localStorage for tests

--- scripts/run-node-tests.js
+++ scripts/run-node-tests.js
@@ -2,8 +2,7 @@
 
 // simulate localStorage - must be done before importing tests
 var { Global } = require('../src/util')
-var LocalStorage = require('node-localstorage').LocalStorage
-Global.localStorage = new LocalStorage('/tmp/store.js-test')
+Global.localStorage = require('localStorage')
 
 // Import and run tests
 var tests = require('../tests/tests')
