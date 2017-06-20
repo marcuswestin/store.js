14dac295388cb1743d7a724c14fa30a3624c1c42
Marcus Westin <marcus.westin@gmail.com>
2017-06-20 08:44:24 -0400

    Clean up bugs/all.js

--- tests/bugs/all.js
+++ tests/bugs/all.js
@@ -1,14 +1,14 @@
 test.group('bugs', function() {
-	test("gh-215: Expire plugin doesn't factor custom namespaces", function() {
+	test('gh-215: "Expire plugin doesn\'t factor custom namespaces"', function() {
 		require('./gh-215')
 	})
-	test("gh-235: Expire and Events plugins conflict with each other", function() {
+	test('gh-235: "Expire and Events plugins conflict with each other"', function() {
 		require('./gh-235')
 	})
-	test("gh-236", function() {
+	test('gh-236: "No supported storage has been added"', function() {
 		require('./gh-236')
 	})
-	test("gh-239", function() {
+	test('gh-239: "No supported storage has been added! Add one"', function() {
 		require('./gh-239')
 	})
 })
